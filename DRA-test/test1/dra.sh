59f175b1fe87b124b42fdb1283f79eb7dc481f7c
Leruiz <1569568063@qq.com>
2017-11-09 16:07:06 +0800

    add direction "DRA-test/" to place test files

---
+++ DRA-test/test1/dra.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+#test 1
+
+fil="DRA-test/test1/record1.txt"
+filawk="DRA-test/throughput.awk"
+filtcll="DRA-test/test1/dra-tcl-group.tcl"
+
+echo "test1 k = 2" >> $fil
+
+k = 2
+
+echo "num_node = 100  idle_time = 6" >> $fil
+ns $filtcll poisson  $k  100 6
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 50  idle_time = 2.5" >> $fil
+ns $filtcll poisson  $k  50 2.5
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 100  idle_time = 2.5" >> $fil
+ns $filtcll poisson  $k  100 2.5
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 150  idle_time = 2.5" >> $fil
+ns $filtcll poisson  $k  150 2.5
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 200  idle_time = 2.5" >> $fil
+ns $filtcll poisson  $k  200 2.5
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 300  idle_time = 2.5" >> $fil
+ns $filtcll poisson  $k  300 2.5
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 200  idle_time = 1.5" >> $fil
+ns $filtcll poisson  $k  200 1.5
+awk -f $filawk out.tr >> $fil
+
+echo "num_node = 300  idle_time = 1.5" >> $fil
+ns $filtcll poisson  $k  300 1.5
+awk -f $filawk out.tr >> $fil
+
