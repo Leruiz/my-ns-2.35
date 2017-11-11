b796f3d43f122017e6b77d30941716e02927ba30
Leruiz <1569568063@qq.com>
2017-11-11 09:34:33 +0800

    modify the packet length to be a random number add a test direction for this modification "DRA-test/test6/"

--- common/packet.h
+++ common/packet.h
@@ -528,9 +528,10 @@ public:
 	//Replica* rep;
 
 //*************************************************************************************
-	Packet() : bits_(0), data_(0), ref_count_(0), next_(0), dcsize(150), half_retrans_counter(0),
-			re_dcsize(0),loca_stamp1(0),loca_stamp2(0),isfirst(true), retrans_(0)
+	Packet() : bits_(0), data_(0), ref_count_(0), next_(0), //dcsize(150),
+			half_retrans_counter(0), re_dcsize(0),loca_stamp1(0),loca_stamp2(0),isfirst(true), retrans_(0)
 	{
+		dcsize = 80 + ((int)(Random::uniform() * 80)) % 80;//random packet length
 		re_dcsize=dcsize;
 		//rep = new Replica (dcsize);
 		own_uid_ = uid_;
